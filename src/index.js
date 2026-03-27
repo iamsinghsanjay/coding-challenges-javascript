0613db4b7492cd3577b167f37f3e7957b7cf8de5
Sanjay Singh <isanjaypsingh@gmail.com>
2026-03-28 01:16:42 +0530

    rename

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ console.log("Testing...");
 // import "./challengesOne/ReverseAnArrayWithoutusingReverse.js"
 // import "./challengesOne/CheckIfAnArrayIsPalindrome.js"
 // import "./challengesOne/MaximumDifferenceBetweenAnyTwoElements.js"
-import "./challengesOne/SumOfElementsAtEvenIndicesOnly.js"
+// import "./challengesOne/SumOfElementsAtEvenIndicesOnly.js"
